09c6f4f67f272e9bdfd4af482989a6ef6d3bd287
Chehan Lasindu <clmallikage001@gmail.com>
2026-09-17 23:37:07 +0530

    Fixed-Backend-Errors

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ allowed_origins.extend(FRONTEND_URLS)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
+    allow_origin_regex=r"https://sri-lanka-al-exam[a-z0-9-]*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
